6d30004f05e15b7c013567ffc53c73a06650bb49
kagaminnnnn <toida2003@gmail.com>
2023-11-02 14:13:04 +0900

    add paint_at

--- field_map.py
+++ field_map.py
@@ -5,11 +5,13 @@ class FieldMap:
     field_height = 20
     
     def __init__(self) -> None:
-        self.field_map = np.zeros((self.field_height
-                                   , self.field_width))
+        self.field_map = np.zeros((self.field_height, self.field_width))
     
     def get_2d_map(self):
         return self.field_map
     
     def get_flatten_map(self):
         return self.field_map.flatten()
+    
+    def paint_at(self, x, y, to):
+        self.field_map[y][x] = to
