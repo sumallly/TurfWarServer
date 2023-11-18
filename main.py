0fb7c95ec8c11603f8818719edbca4a4d75df8ea
kaga1minn0nnn4 <toida2003@gmail.com>
2023-11-18 12:52:57 +0900

    add ServerResponse

--- main.py
+++ main.py
@@ -11,30 +11,30 @@ def callback_accept(client, addr):
     print(addr)
     gs.register(addr)
     session_id, player_num = gs.inquiry(addr)
-    
+
     if gs.get_join_num(session_id) == 1:
         games.append(TurfWarGame())
-    
+
     try:
         while True:
             if gs.get_join_num(session_id) != 2:
                 continue
-            
+
             field_map = games[session_id].get_map()
-            
+
             client.send(field_map.encode())
             responce =  client.recv(256).decode()
-            
+
             games[session_id].step(responce)
-            
-            
+
+
     except BrokenPipeError:
         print(f"close id({session_id})")
         gs.remove_from_id(session_id)
         client.close()
-        
 
-def main():    
+
+def main():
     tcp_server = TcpServer("127.0.0.1", 8000)
     tcp_server.register_callback(callback_accept)
 
