0b32cc407c4d6a8d5846942c45ae78096cdaccf4
kaga1minn0nnn4 <toida2003@gmail.com>
2023-11-20 20:52:35 +0900

    add display_map in test_client.py

--- test_client.py
+++ test_client.py
@@ -7,6 +7,14 @@ target_ip = "127.0.0.1"
 target_port = 8000
 buffer_size = 4096
 
+def display_map(raw):
+    w = 30
+    h = 20
+    for i in range(h):
+        for j in range(w):
+            print(raw[i*w+j], end="")
+        print()
+
 # 1.ソケットオブジェクトの作成
 tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -14,6 +22,9 @@ tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcp_client.connect((target_ip,target_port))
 while True:
     response = tcp_client.recv(buffer_size).decode()
-    if response == "":
-        continue
-    print("[*]Received a response : {}".format(response))
+    res_msgs = response.split(",")
+    display_map(res_msgs[0])
+
+    print("input=")
+    cli_msg = input()
+    tcp_client.send(cli_msg.encode())
