ce811f291de38ed2fc9da562e369da50dbc30984
sumallly <referlance@gmail.com>
2023-11-20 15:48:48 +0900

    make function * init (make flat map) * get raw map data * get string map

--- field_map.py
+++ field_map.py
@@ -1,37 +1,32 @@
-import numpy as np
-
 class FieldMap:
-    field_width = 6
-    field_height = 6
+    # empty 0, obstacle 1, item 2, player 11-19
 
-    obstacle_block = "*"
-    empty_block = " "
+    def __init__(self, worldtype = "flat", mapsize = {'x':30, 'y':20}) -> None:
+        self.map = [[0 for j in range(mapsize['x'])] for i in range(mapsize['y'])]
+        for row in self.map:
+            row[0] = row[mapsize['x'] - 1] = 1
 
-    def __init__(self, init) -> None:
-        self.field_map = init
+        if worldtype == "flat":
+            pass
+        # elif worldtype = "def"
 
-    def get_2d_map(self):
-        return self.field_map
+    def get_map_raw(self):
+        return self.map
 
-    def get_flatten_map(self):
-        return self.field_map.flatten()
+    def get_map_sendable(self):
+        mapstr = ""
+        for row in self.map:
+            for unit in row:
+                if unit == 1:
+                    mapstr += '*'
+                elif unit == 2:
+                    mapstr += '?'
+                elif unit > 10:
+                    # process by player
+                    mapstr += 'x'
+                else:
+                    mapstr += ' '
+        return mapstr
 
-    def paint_at(self, x, y, to):
-        if not(0 <= x < self.field_width) or not(0 <= y < self.field_height):
-            raise ValueError("This point is unavailable.")
-
-        if self.field_map[y][x] == self.obstacle_block:
-            raise ValueError(f"There is obstacle block at ({x}, {y}).")
-
-        self.field_map[y][x] = to
-
-    @classmethod
-    def create_flatmap(cls):
-        flatmap = np.array([["*", "*", "*", "*", "*", "*"],
-                            ["*", " ", " ", " ", " ", "*"],
-                            ["*", " ", " ", " ", " ", "*"],
-                            ["*", " ", " ", " ", " ", "*"],
-                            ["*", " ", " ", " ", " ", "*"],
-                            ["*", "*", "*", "*", "*", "*"]])
-        fm = FieldMap(flatmap)
-        return fm
+    # def paint
+    # def can be painted
