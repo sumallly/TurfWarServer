968c1e9af1a8c80ed52c47972338090b5276890a
sumallly <referlance@gmail.com>
2023-11-27 01:48:23 +0900

    Fix associated with migration to ensure compatibility

--- field_map.py
+++ field_map.py
@@ -1,46 +1,50 @@
+from cmath import pi
 from random import randint
-
+from operate_map import OperateField
 
 class FieldMap:
     # empty 0, obstacle 1, item 2, player 11-
 
-    def __init__(self, worldtype = "flat", fieldsize = {'x':30, 'y':20}) -> None:
-        self.d = {"empty":0, "obstacle":1, "item":2}
+    @classmethod
+    def get_field_type(cls) -> list:
+        return OperateField.get_field_type()
+
+    def __init__(self, fieldtype = None, fieldsize = {'x':30, 'y':20}) -> None:
         self.position = {}
-        self.symbol = {}
+        self.symbols = {}
         self.userNum = 0
         
-        self.field = [[0 for j in range(fieldsize['x'])] for i in range(fieldsize['y'])]
-        for row in self.field:
-            row[0] = row[fieldsize['x'] - 1] = 1
-
-        if worldtype == "flat":
-            pass
-        # elif worldtype = "def"
+        if worldtype is None:
+            worldtype = 0
+        
+        self.operator = OperateField(fieldtype, fieldsize)
+        self.d = self.operator.d
 
     def get_map_raw(self) -> list:
-        return self.field
+        return self.operator.field
+
+    def get_map_sendable(self) -> str:
+        return self.operator.get_map_sendable(self.symbols)
 
     def get_map_sendable(self) -> str:
         fieldstr = ""
-        for row in self.field:
+        for row in self.operator.field:
             for unit in row:
                 if unit == 1:
                     fieldstr += '*'
                 elif unit == 2:
                     fieldstr += '?'
                 elif unit > 10:
-                    fieldstr += self.symbol[unit]
+                    fieldstr += self.symbols[unit]
                 else:
                     fieldstr += ' '
         return fieldstr
     
     def add_player(self, ID:str, symbol:str) -> None:
         self.d[ID] = self.userNum + 11
-        self.position[self.d[ID]] = [randint(1, 19), randint(1, 29)]
-            # [y, x] make func init position
-        self.symbol[self.d[ID]] = symbol
-        self.__paint_by_position(ID, self.position[self.d[ID]], 0)
+        self.position[self.d[ID]] = self.operator.get_init_position()
+        self.symbols[self.d[ID]] = symbol
+        self.operator.paint_by_position(self.d[ID], self.position[self.d[ID]], 0)
         self.userNum += 1
     
     def get_user_position(self, ID:str) -> list:
@@ -50,21 +54,13 @@ class FieldMap:
         self.position[self.d[ID]] = position
     
     def get_user_symbol(self, ID:str) -> str:
-        return self.symbol[self.d[ID]]
+        return self.symbols[self.d[ID]]
     
     def overwrite_user_symbol(self, ID:str, symbol:str) -> None:
-        self.symbol[self.d[ID]] = symbol
+        self.symbols[self.d[ID]] = symbol
     
     def get_can_be_painted_direction(self, ID:str):
-        return self.__can_be_painted(self.position[self.d[ID]])
-    
-    def __can_be_painted(self, position:list) -> list:
-        y = position[0]
-        x = position[1]
-        return [self.field[y-1][x] != self.d["obstacle"],
-                self.field[y][x-1] != self.d["obstacle"],
-                self.field[y][x+1] != self.d["obstacle"],
-                self.field[y+1][x] != self.d["obstacle"]]
+        return self.operator.can_be_painted(self.position[self.d[ID]])
     
     def paint_by_direction(self, ID:str, direction:str, item:int=0) -> int:
         pos = self.position[self.d[ID]]
@@ -79,17 +75,4 @@ class FieldMap:
         else:
             # invalid direction charactor
             pass
-        return self.__paint_by_position(ID, pos, item)
-
-    def __paint_by_position(self, ID:str, position:list, item:int) -> int:
-        y = position[0]
-        x = position[1]
-        status = 0
-        if self.field[y][x] == self.d["obstacle"]:
-            status =  -1
-        else:    
-            if self.field[y][x] == self.d["item"]:
-                # process by item
-                status += 1
-            self.field[y][x] = self.d[ID]
-        return status
+        return self.operator.paint_by_position(self.d[ID], pos, item)
